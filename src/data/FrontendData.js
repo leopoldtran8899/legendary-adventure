faef8f5c04ae671dcc41f9a9ffd7bd1e1d89096c
Leopold Tran <leopoldtran8899@gmail.com>
2022-11-12 23:53:53 +1100

    add projects to list

--- src/data/FrontendData.js
+++ src/data/FrontendData.js
@@ -17,6 +17,14 @@ export const ProjectList = [
     github: 'https://github.com/leopoldtran8899/pokemon-gallery',
     demo: 'https://leopoldtran8899.github.io/pokemon-gallery'
   },
+  {
+    id: 2,
+    title: 'Dashboard',
+    tech: ['react', 'tailwind', 'next'],
+    desc: 'WIP project. A responsive web page show client\'s information such as IP, weather focas, etc. This project uses WeatherOpenAPI (https://openweathermap.org/) and AbstractAPI (https://www.abstractapi.com/api/ip-geolocation-api)',
+    github: 'https://github.com/leopoldtran8899/weather-board',
+    demo: 'https://leopoldtran8899.github.io/weather-board/'
+  },
   
 
 ]
